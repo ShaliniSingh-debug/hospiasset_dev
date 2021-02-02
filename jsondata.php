79f7e687f134ffb674480705239b49de025636fd
ShaliniSingh-debug <senger.shalinisingh@gmail.com>
2021-02-02 09:38:57 +0530

    first commit

---
+++ jsondata.php
@@ -0,0 +1,160 @@
+<?php
+$json= '{
+  "elements" : [
+	
+	{
+	  "name" : "EMRI",
+	  "img" : "/xray.jpg",
+	  "online": "15-01-2021 17:21",
+	  "messages" : [
+		{
+		  "text" : "Hello! Was tut sich in Hagenberg?",
+		  "time" : "15-01-2021 16:45",
+		  "type" : false
+		},
+		{
+		  "text" : "Hello Shalini :) Wie immer sehr viel Stress mit den Projekten, aber sonst ist alles super!",
+		  "time" : "15-01-202116:48",
+		  "type" : true
+		},
+		{
+		  "text" : "Ja ich weiÃŸ, aber nur noch ein paar Tage und dann sind eh Weihnachtsferien. Das musst halt noch durchhalten.",
+		  "time" : "15-01-202116:55",
+		  "type" : false
+		},
+		{
+		  "text" : "Hast eh recht! Aber bin echt froh, wenn ich mal wieder nach Hause komme!",
+		  "time" : "15-01-2021 17:01",
+		  "type" : true
+		},
+		{
+		  "text" : "Wir freuen uns auch alles schon, wenn du wieder heim kommst :*",
+		  "time" : "15-01-2021 17:06",
+		  "type" : false
+		},
+		{
+		  "text" : "Kannst du mich dann eventuell in Linz abholen?",
+		  "time" : "15-01-2021 17:15",
+		  "type" : true
+		},
+		{
+		  "text" : "Sicher, ruf einfach an wenn du weiÃŸt wenn du zum holen bist.",
+		  "time" : "15-01-2021 17:19",
+		  "type" : false
+		},
+		{
+		  "text" : "Okay super danke! Dann bis Freitag :)",
+		  "time" : "15-01-2021 17:20",
+		  "type" : true
+		},
+		{
+		  "text" : "Bis Freitag :)",
+		  "time" : "15-01-2021 17:21",
+		  "type" : false
+		}
+	  ]
+	},
+	{
+	  "name" : "EMRI",
+	  "img" : "/xray.jpg",
+
+	  "members" : [
+		{
+		  "contact" : "0"
+		},
+		{
+		  "contact" : "0"
+		},
+		{
+		  "contact" : "0"
+		}
+	  ],
+	  "messages" : [
+		{
+		  "text" : "breakdown",
+		  "name" : "Uday - Assigned",
+		  "time" : "15-01-2021 17:30",
+		  "type" : true
+		},
+		{
+		  "text" : "breakdown",
+		  "name" : "Uday  - started",
+		  "time" : "15-01-2021 17:32",
+		  "type" : false
+		},
+		{
+		  "text" : "spare parts required",
+		  "name" : "Uday - in progress",
+		  "time" : "15-01-2021 17:33",
+		  "type" : true
+		},
+		
+		{
+
+		  "text" : "Spare parts approval",
+		  "name" : "Prasad - approved",
+		  "time" : "15-01-2021 17:33",
+		  "type" : false
+		},
+		
+		{
+		  "text" : "Success",
+		  "name" : "Uday - completed",
+		  "time" : "15-01-2021 17:44",
+		  "type" : false
+		}
+	  ]
+	},
+	{
+	  "name" : "EMRI",
+"img" : "/xray.jpg",
+	  "members" : [
+		{
+		  "contact" : "0"
+		},
+		{
+		  "contact" : "0"
+		},
+		{
+		  "contact" : "0"
+		}
+	  ],
+	   "messages" : [
+		{
+		  "text" : "breakdown",
+		  "name" : "Uday - Assigned",
+		  "time" : "17-01-2021 17:30",
+		  "type" : true
+		},
+		{
+		  "text" : "breakdown",
+		  "name" : "Uday  - started",
+		  "time" : "17-01-2021 17:32",
+		  "type" : false
+		},
+		{
+		  "text" : "spare parts required",
+		  "name" : "Uday - in progress",
+		  "time" : "17-01-2021 17:33",
+		  "type" : true
+		},
+		
+		{
+
+		  "text" : "Spare parts approval",
+		  "name" : "Prasad - approved",
+		  "time" : "17-01-2021 17:33",
+		  "type" : false
+		},
+		
+		{
+		  "text" : "Success",
+		  "name" : "Uday - completed",
+		  "time" : "17-01-2021 17:44",
+		  "type" : false
+		}
+	  ]
+	}
+  ]
+}';
+echo  $json;
